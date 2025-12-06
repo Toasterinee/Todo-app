4ab4ff357fde1e7eb5cee619f1f4f539e6a93e63
Eugene Shevnin <toasterinee@gmail.com>
2025-12-06 17:24:50 +0300

    db connection logic set and other things

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	todo "github.com/Toasterinee/Todo-app"
 	"github.com/Toasterinee/Todo-app/pkg/handler"
 	"github.com/Toasterinee/Todo-app/pkg/repository"
 	"github.com/Toasterinee/Todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured while initializing config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.database"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE todo_lists (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE users_lists (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+    list_id INTEGER REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE todo_items (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255),
+    done BOOLEAN DEFAULT FALSE NOT NULL,
+    list_id INTEGER REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE lists_items (
+    id SERIAL PRIMARY KEY,
+    list_id INTEGER REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL,
+    item_id INTEGER REFERENCES todo_items(id) ON DELETE CASCADE NOT NULL
+);
